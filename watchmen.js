ddf378d32d2d7ce43c041de226129c2417300de2
Iván Loire <ivan@iloire.com>
2011-12-02 23:35:54 +0100

    refactoring, post implementation, colors

--- watchmen.js
+++ watchmen.js
@@ -1,14 +1,50 @@
+/*
+Watchmen, and HTTP nonitor for node.js
+
+Copyright (c) 2011 Ivan Loire (twitter: @ivanloire)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
 var config = require('./config.js')
 var http = require('http');
 var postmark= require('./postmark');
-
+var sys = require('sys');
+var colors = require('colors');
+ 
 function processRequest (url_conf, callback){
+	var headers = {
+	    'Host': url_conf.host.host
+	};
+	
+	if (url_conf.method == 'post'){
+		headers['Content-Type'] = url_conf.content_type
+		headers['Content-Length'] = JSON.stringify(url_conf.input_data || '').length
+	}
 	
 	var client = http.createClient(url_conf.host.port || 80, url_conf.host.host);
-	var request = client.request(url_conf.method, url_conf.url, {'Host': url_conf.host.host });
+	var request = client.request(url_conf.method, url_conf.url, headers);
 	
 	client.addListener('error', function(connectionException){
-		callback(connectionException.errno, null, null);
+		callback(connectionException.errno || 'Error establishing connection' , null, null);
+		log_error ('Error: ' + connectionException)
 	});
 	
 	request.on('response', function(response) {
@@ -24,53 +60,58 @@ function processRequest (url_conf, callback){
 		});
 		
 		response.on('error', function(e) {
+			log_error ('Error on response :' + url_conf.host)
 			callback(e.message, null, null)
 		});
 	});
 	
 	request.on('error', function(e) {
+		log_error ('E	rror on request :' + url_conf.host)
 		callback(e.message, null, null)
 	});
 	
-	request.write("");
+	request.write(JSON.stringify(url_conf.input_data) || '');
 	request.end();
 }
+
+function log_info (str){ sys.puts (str.grey) }
+function log_ok (str){ sys.puts (str.green) }
+function log_error (str){ sys.puts (str.red) }
+function log_warning (str){ sys.puts (str.yellow.bold) }
 
 function sendEmail (to, subject, body){
 	postmark.sendEmail(
 	{
-		'From' : config.postmark.From,
+		'From' : config.notifications.postmark.From,
 		'To': to,
 		'Subject': subject,
-		'TextBody': body }, config.postmark.Api_key, function(err, data) {
+		'TextBody': body }, config.notifications.postmark.Api_key, function(err, data) {
 
 		if (err) {
-			console.log('Error sending email: ' + JSON.stringify(err))
+			log_error('Error sending email: ' + JSON.stringify(err))
 		} else {
-			console.log('Email sent successfully')
+			log_ok('Email sent successfully')
 		}
 	})
 }
 
 function query_url(url_conf){
 	var host_conf = url_conf.host
-		
-	processRequest(url_conf, function(err, body, response){
+	var url_info = host_conf.host + ':'+ host_conf.port  + url_conf.url + ' [' + url_conf.method + ']'
+	processRequest(url_conf, function(request_err, body, response){
 		var error, next_attempt_secs
-		if (!err){
+		if (!request_err){
 			if (response.statusCode != url_conf.expected.statuscode){
-				error = 'we expected status code ' + url_conf.expected.statuscode + ' at ' + url_conf.url + ' but got ' + response.statusCode
+				error = 'FAILED! expected status code :' + url_conf.expected.statuscode + ' at ' + url_conf.url + ' but got ' + response.statusCode
 			}
 			else if (url_conf.expected.contains && body.indexOf(url_conf.expected.contains)==-1){
-				error = 'expected text ' + url_conf.expected.contains + ' but no found '
+				error = 'FAILED! expected text "' + url_conf.expected.contains + '" but it wasn\'t found'
 			}
 		}
 		else
-			error = 'Connection error: ' + err
+			error = 'Connection error when processing request: ' + request_err
 		
-		console.log (host_conf.host + ':'+ host_conf.port + ', url: "' +  url_conf.url + '" : ' + (error || ' response ok!'))
-		
-		if (error){			
+		if (error){
 			if (url_conf.attempts==undefined)
 				url_conf.attempts = 0
 			else if (url_conf.attempts < (url_conf.retry_in || url_conf.host.retry_in).length-1){
@@ -80,24 +121,27 @@ function query_url(url_conf){
 			if ((url_conf.attempts >= (url_conf.notify_after_failed_ping || host_conf.notify_after_failed_ping)) && config.notifications.Enabled){
 				sendEmail(
 					host_conf.alert_to || config.notifications.To, 
-					host_conf.host + ':'+ host_conf.port  + url_conf.url + ' is down!',
+					url_info + ' is down!',
 					error);
 			} else{
-				console.log ('Notification disabled or not triggered this time');
+				log_info ('Notification disabled or not triggered this time');
 			}
 			
 			next_attempt_secs = (url_conf.retry_in || url_conf.host.retry_in) [url_conf.attempts] * 60;
-			console.log ('Site down! Retrying in ' + next_attempt_secs / 60 + ' minutes..' )
+			log_error (url_info + ' down!. Error: ' + error + '. Retrying in ' + next_attempt_secs / 60 + ' minute(s)..')
 		}
 		else { //site up. queue next ping
 			next_attempt_secs = (url_conf.ping_interval || url_conf.host.ping_interval);
-			if (url_conf.attempts){
-				//site is up again!
+			if (url_conf.attempts){ //site is up again!
 				url_conf.attempts = 0
 				sendEmail(
 					host_conf.alert_to || config.notifications.To,
-					host_conf.host + ':'+ host_conf.port  + url_conf.url + ' is back up!',
+					url_info + ' is back up!',
 					'site is up again!');
+				log_ok (url_info +' is back!')
+			}
+			else{
+				log_ok (url_info + ' responded OK!')
 			}
 		}
 		
@@ -107,21 +151,22 @@ function query_url(url_conf){
 
 /*main*/
 
-console.log ('Starting Watchmen...')
+log_info ('starting watchmen...')
+log_info ('reading configuration and queuing hosts for pinging...')
 
 for (var i=0; i<config.hosts.length;i++){
 	var host = config.hosts[i];
 	if (host.enabled){
-		console.log ('Monitoring ' + host.name + ':')
+		log_info ('Monitoring ' + host.name + ':')
 		for (var u=0;u<config.hosts[i].urls.length;u++){
 			var url = host.urls[u]
 			url.host = host
 			var ping = (url.ping_interval || host.ping_interval) 
-			console.log (' -- ping "' + url.url + '" every ' + ping + ' seconds...')
+			log_info (' -- Queuing "' + url.url + '".. ping every ' + ping + ' seconds...')
 			setTimeout (query_url, ping * 1000, url);
 		}
 	}
 	else{
-		console.log ('Skipping ' + host.name + '...')
+		log_warning ('Skipping ' + host.name + ' (disabled entry)...')
 	}
 }
