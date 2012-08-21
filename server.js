57146a6c47f84b8c1fb29816e38a8f1f23de1c46
Iván Loire <ivan@iloire.com>
2012-08-21 09:15:36 -0300

    removed references to coloured console messages. code cleaning

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 var config = require('./config/general');
-var util = require('util');
 var email_service = require ('./lib/email');
-var services_manager = require ('./lib/services');
-
 var storage_factory = require ('./lib/storage/storage_factory');
 var ping_service = require ('./lib/ping_services/http'); //http for now
 
-var services = services_manager.load_services();
-
-var error   = '\u001b[31m'; //red
-var warning   = '\u001b[39m'; //yellow
-var ok  = '\u001b[32m'; //green
-
-var reset = '\u001b[0m';
-
-function log_ok (str){
-	console.log(ok + str + reset);
-}
-
-function log_error (str){
-	console.log(error + str + reset);
-}
-
-function log_warning (str){
-	console.log(warning + str + reset);
-}
+var services = require ('./lib/services').load_services();
 
 var WatchMen = require ('./lib/watchmen');
 var storage = storage_factory.get_storage_instance();
@@ -41,7 +20,7 @@ watchmen.on('service_error', function(service, state){
 	var info = service.url_info + ' down!. Error: ' + state.error + '. Retrying in ' +
 			(parseInt(state.next_attempt_secs, 10) / 60) + ' minute(s)..';
 
-	log_error (info);
+	console.log (info);
 	*/
 
 	if (state.prev_state.status != 1 && config.notifications.enabled){
@@ -58,7 +37,7 @@ watchmen.on('service_warning', function(service, state){
 	/*
 	//Do here any additional stuff when you get a warning
 
-	log_warning (service.url_info + ' WARNING (' + state.elapsed_time + ' ms, avg: '
+	console.log (service.url_info + ' WARNING (' + state.elapsed_time + ' ms, avg: '
 			+ state.avg_response_time + ') ## ' + state.warnings + ' warnings');
 	*/
 
@@ -78,7 +57,7 @@ watchmen.on('service_ok', function(service, state){
 	/*
 	//Do here any additional stuff when you get a successful response
 
-	log_ok (service.url_info + ' responded OK! (' + state.elapsed_time + ' milliseconds, avg: '
+	console.log (service.url_info + ' responded OK! (' + state.elapsed_time + ' milliseconds, avg: '
 			+ state.avg_response_time + ')');
   */
 });
