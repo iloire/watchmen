4975562198573ad7400f803e65202d5812957456
Iván Loire <ivan@iloire.com>
2012-08-21 08:59:08 -0300

    Separate folder for express routes

--- webserver/app.js
+++ webserver/app.js
@@ -22,46 +22,14 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-//-------------------------------
-// Url log detail
-//-------------------------------
-app.get('/details', function(req, res){
-	var max = 100;
-	var host = req.query ['host'], url = req.query ['url'], port = req.query['port'];
-	var service = require ('../lib/services').load_services().filter(function(service){
-		return (service.host.host == host && service.url == url && service.host.port == port);
-	});
-	if (!service.length){
-		return res.end('not found');
-	}
+//-----------------------------------------
+// Import routes
+//-----------------------------------------
+require('./routes/reporting').add_routes(app, storage);
 
-	storage.report_one(service[0], function (err, service){
-		res.render('details.html', {
-			title: service.url_info,
-			service: service,
-			critical_events: service.data.events.filter(function(item){return item.type == 'critical';}),
-			warning_events: service.data.events.filter(function(item){return item.type == 'warning';})
-		});
-	});
-});
-
-//-------------------------------
-// List of hosts and url's
-//-------------------------------
-app.get('/', function(req, res){
-	res.render('list.html', {title: 'watchmen'});
-});
-
-//-------------------------------
-// Get list (JSON)
-//-------------------------------
-app.get('/getdata', function(req, res){
-	var services = require ('../lib/services').load_services();
-	storage.report_all(services, function (err, data){
-		res.json(data);
-	});
-});
-
+//-----------------------------------------
+// Start server
+//-----------------------------------------
 var port = parseInt(process.argv[2], 10) || 3000;
 app.listen(port);
 if (app.address())
