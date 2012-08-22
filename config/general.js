5a602119d5e79116d8997c636b54cc697d3917fa
Iván Loire <ivan@iloire.com>
2012-08-22 11:24:10 -0300

    format

--- config/general.js
+++ config/general.js
@@ -1,8 +1,10 @@
-module.exports.notifications = {
-	enabled: false, //if disabled, no email will be sent (just console messages)
-	to: ['ivan@iloire.com'], //default notification list if no alert_to is specified for host or url
-	postmark : {
-		from: 'ivan@iloire.com',
-		api_key : 'your-postmark-key-here'
+module.exports = {
+	'notifications' : {
+		enabled: false, //if disabled, no notifications will be sent
+		to: ['ivan@iloire.com'], //default notification list if no alert_to is specified for host or url
+		postmark : {
+			from: 'ivan@iloire.com',
+			api_key : 'your-postmark-key-here'
+		}
 	}
 };
