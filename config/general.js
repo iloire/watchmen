f82bf42431825e0d1c1a08588a536f22ea01ae5c
Ivan Loire <iloire@atlassian.com>
2014-09-21 12:55:24 +1000

    Make remove_events_older_than_seconds config options inherit from host and config if it is not defined

--- config/general.js
+++ config/general.js
@@ -1,4 +1,5 @@
 module.exports = {
+
   'notifications' : {
     enabled: false, //if disabled, no notifications will be sent
     to: ['your-email@domain.com'], //default notification list if no alert_to is specified for host or url
@@ -6,5 +7,7 @@ module.exports = {
       from: 'your-email@domain.com',
       api_key : 'your-postmark-key-here'
     }
-  }
+  },
+
+  remove_events_older_than_seconds : 60 * 60 * 24 * 10, // 10 days default configuration
 };
