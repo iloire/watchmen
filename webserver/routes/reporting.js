d405fc8ddd0d7c3dfc8a45036166f48ce7cd9a78
Ivan Loire <iloire@atlassian.com>
2014-09-21 18:47:48 +1000

    Display how much back in time we store events

--- webserver/routes/reporting.js
+++ webserver/routes/reporting.js
@@ -1,24 +1,25 @@
-var services_loader = require ('../../lib/services')
+var services_loader = require ('../../lib/services');
+var moment = require('moment');
 
 module.exports.add_routes = function (app, storage){
   //-------------------------------
   // Url log detail
   //-------------------------------
   app.get('/details', function(req, res){
-    var max = 100;
-    var host = req.query ['host'], service_name = req.query ['service'];
+
     var service = services_loader.load_services().filter(function(service){
-      return (service.host.name === host && service.name === service_name);
-    });
-    if (!service.length){
+      return (service.host.name === req.query ['host'] && service.name === req.query ['service']);
+    })[0];
+
+    if (!service){
       return res.end('not found');
     }
 
-    storage.report_one(service[0], function (err, service){
-     
+    storage.report_one(service, function (err, service){
       res.render('details.html', {
         title: service.url_info,
         service : service,
+        eventsSince : moment (+new Date() - service.remove_events_older_than_seconds * 1000),
         status: service.data ? service.data.status : 'unavailable', // no data collected yet
         critical_events: service.data ? service.data.events.filter(function(item){return item.type == 'critical';}) : [],
         warning_events: service.data ? service.data.events.filter(function(item){return item.type == 'warning';}) : []
