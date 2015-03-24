b32b9b2d7b4a26932087d16fe11c00a27b230a0d
Ivan Loire <iloire@atlassian.com>
2015-03-24 23:40:07 +1100

    Add prefix to env variables

--- config/general.js
+++ config/general.js
@@ -3,8 +3,8 @@ module.exports = {
   public_host_name: 'http://watchmen.letsnode.com/', // required for OAuth dance
 
   auth: {
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '<Create credentials from Google Dev Console>',
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || '<Create credentials from Google Dev Console>'
+    GOOGLE_CLIENT_ID: process.env.WATCHMEN_GOOGLE_CLIENT_ID || '<Create credentials from Google Dev Console>',
+    GOOGLE_CLIENT_SECRET: process.env.WATCHMEN_GOOGLE_CLIENT_SECRET || '<Create credentials from Google Dev Console>'
   },
 
   'notifications' : {
