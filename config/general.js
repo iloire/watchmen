d3f72645901b824e0eeaf4b8c45f0b116fae919c
Ivan Loire <iloire@atlassian.com>
2015-03-24 22:08:35 +1100

    User passport-google-oauth

--- config/general.js
+++ config/general.js
@@ -2,6 +2,11 @@ module.exports = {
 
   public_host_name: 'http://watchmen.letsnode.com/', // required for OAuth dance
 
+  auth: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '<Create credentials from Google Dev Console>',
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || '<Create credentials from Google Dev Console>'
+  },
+
   'notifications' : {
     enabled: false, //if disabled, no notifications will be sent
     to: ['your-email@domain.com'], //default notification list if no alert_to is specified for host or url
