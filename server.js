c2e2873397e516baa83077fe8171cfe2b527cda5
Ivan Loire <iloire@atlassian.com>
2015-03-31 20:09:20 +1100

    Refactoring

--- server.js
+++ server.js
@@ -5,20 +5,16 @@ var notificationService = new notificationsFactory();
 var storageFactory = require('./lib/storage/storage_factory');
 var storage = storageFactory.get_storage_instance();
 
-require('./lib/services').load_services(function(err, services){
+var eventHandlers = {
 
-  if (err) {
-    console.error('error loading services'.red);
-    storage.quit();
-    process.exit(0);
-  }
+  /**
+   * When there is an error pinging the service
+   * @param service
+   * @param state
+   */
 
-  var WatchMen = require('./lib/watchmen');
-
-  var watchmen = new WatchMen(services, storage);
-
-  watchmen.on('service_error', function(service, state) {
-    var errorMsg = service.url_info + ' down!. ' + state.error;
+  onServiceError: function(service, state) {
+    var errorMsg = service.url_info + ' down!. ' + state.error.red;
     var retryingMsg = '. retrying in ' + (parseInt(state.next_attempt_secs, 10) / 60) + ' minute(s)..';
 
     console.log (errorMsg + retryingMsg.gray);
@@ -26,33 +22,83 @@ require('./lib/services').load_services(function(err, services){
     if (state.prev_state.status === 'success') {
       notificationService.sendServiceDownAlert(service, state.error);
     }
-  });
+  },
 
-  watchmen.on('service_warning', function(service, state) {
+  /**
+   * Warning alert pinging the service
+   * @param service
+   * @param state
+   */
+
+  onServiceWarning: function (service, state) {
     /*
-    // Do here any additional stuff when you get a warning
+     // Do here any additional stuff when you get a warning
 
-    console.log (service.url_info + ' WARNING (' + state.elapsed_time + ' ms, avg: '
-        + state.avg_response_time + ') ## ' + state.warnings + ' warnings');
-    */
-  });
+     console.log (service.url_info + ' WARNING (' + state.elapsed_time + ' ms, avg: '
+     + state.avg_response_time + ') ## ' + state.warnings + ' warnings');
+     */
+  },
 
-  watchmen.on('service_back', function(service, state) {
+  /**
+   * Service is back up online
+   * @param service
+   * @param state
+   */
+
+  onServiceBack: function (service, state) {
     notificationService.sendServiceBackAlert(service);
-  });
+  },
 
-  watchmen.on('service_ok', function(service, state) {
+  /**
+   * Service is responding correctly (pretty verbose output)
+   * @param service
+   * @param state
+   */
+
+  onServiceOk: function (service, state) {
+
     /*
-    console.log (service.url_info + ' responded OK! (' + state.elapsed_time + ' milliseconds, avg: '
-        + state.avg_response_time + ')');
-    */
-  });
+    var serviceOkMsg = service.url_info + ' responded ' + 'OK!'.green;
+    var responseTimeMsg = state.elapsed_time + ' milliseconds, avg: '
+        + state.avg_response_time;
 
-  //----------------------------------------------------
-  // Start watchmen
-  //----------------------------------------------------
+    console.log (serviceOkMsg, responseTimeMsg.gray);
+    */
+  }
+};
+
+function start (services, storage) {
+
+  var WatchMenFactory = require('./lib/watchmen');
+
+  var watchmen = new WatchMenFactory(services, storage);
+
+  watchmen.on('service_error', eventHandlers.onServiceError);
+  watchmen.on('service_warning', eventHandlers.onServiceWarning);
+  watchmen.on('service_back', eventHandlers.onServiceBack);
+  watchmen.on('service_ok', eventHandlers.onServiceOk);
+
   watchmen.start();
+
   console.log('watchmen monitor started'.gray);
+
+  // notification services info:
+  notificationService.getEnabledServices().forEach(function(s){
+    console.log('', s.name, ' notification service is enabled'.gray);
+    var configErrors = s.checkConfiguration();
+    if (configErrors) {
+      console.error(configErrors.red);
+      exit(1);
+    }
+  });
+}
+
+require('./lib/services').load_services(function(err, services){
+  if (err) {
+    console.error('error loading services'.red);
+    exit(1);
+  }
+  start(services, storage);
 });
 
 
@@ -72,6 +118,10 @@ process.on('uncaughtException', function(err) {
 
 process.on('SIGINT', function () {
   console.log('stopping watchmen..'.gray);
-  storage.quit();
-  process.exit(0);
+  exit(0);
 });
+
+function exit (code) {
+  storage.quit();
+  process.exit(code);
+}
