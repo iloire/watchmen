671db1d7235d1ca778d55aae548953664fc77b76
Ivan Loire <iloire@atlassian.com>
2015-04-05 16:23:22 +1000

    Cache services

--- webserver/public/js/services.js
+++ webserver/public/js/services.js
@@ -4,15 +4,51 @@
 
     /* Services */
 
+    var EXPIRATION = 10 * 1000; //ms
+    var SERVICES_LIST_CACHE_KEY = 'services-cache';
+
     var watchmenServices = angular.module('watchmenServices', ['ngResource']);
 
-    watchmenServices.factory('Service', ['$resource',
-        function($resource){
-            return $resource('services?host=:host&service=:service', {}, {
-                query: {
-                    method:'GET',
-                    isArray:true
+    watchmenServices.factory('Service', ['$resource', '$cacheFactory',
+        function($resource, $cacheFactory) {
+
+            var cache = $cacheFactory('watchmen-Services');
+
+            return {
+                getAll: function(cb) {
+                    var services = cache.get(SERVICES_LIST_CACHE_KEY);
+                    if (services) {
+                        if (services.expiration < +new Date()){
+                            services = null;
+                        }
+                        else {
+                            return cb(services.data);
+                        }
+                    }
+
+                    if (!services) {
+                        services = $resource('services', {}, {
+                            query: {
+                                method: 'GET',
+                                isArray: true
+                            }
+                        }).query(cb);
+                        cache.put(SERVICES_LIST_CACHE_KEY, {
+                            data: services,
+                            expiration: +new Date() + EXPIRATION
+                        });
+                    }
+                    return services;
+                },
+                getDetails: function(options, cb){
+                    return $resource('services?host=:host&service=:service', {}, {
+                        query: {
+                            method:'GET',
+                            isArray:true,
+                            cache: $cacheFactory.get('$http')
+                        }
+                    }).get(options, cb);
                 }
-            });
+            };
         }]);
 })();
