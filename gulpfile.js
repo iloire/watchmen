8b3e2d25ac48ae80c134dc4a10689f47d958003e
Ivan Loire <iloire@atlassian.com>
2015-04-05 16:15:14 +1000

    Minify and concat JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,40 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var watch = require('gulp-watch');
+var plugins = require('gulp-load-plugins')();
+var main_bower_files = require('main-bower-files');
 
 gulp.task('js', function () {
     return gulp.src('./webserver/public/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(uglify())
-        .pipe(concat('app.js'))
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('default'))
+        .pipe(plugins.uglify())
+        .pipe(plugins.concat('app.js'))
         .pipe(gulp.dest('webserver/public/build'));
 });
 
+gulp.task('bower-js', function() {
+    return gulp.src(main_bower_files())
+        .pipe(plugins.filter('*.js'))
+        .pipe(plugins.uglify())
+        .pipe(plugins.concat('vendor.js'))
+        .pipe(gulp.dest('webserver/public/build'));
+});
+
+gulp.task('bower-css', function() {
+    return gulp.src(main_bower_files())
+        .pipe(plugins.filter('*.css'))
+        .pipe(plugins.minifyCss({keepBreaks:true}))
+        .pipe(plugins.concat('vendor.css'))
+        .pipe(gulp.dest('webserver/public/build'));
+});
+
+gulp.task('bower-fonts', function() {
+    return gulp.src(main_bower_files())
+        .pipe(plugins.filter('*.font'))
+        .pipe(plugins.concat('vendor.css'))
+        .pipe(gulp.dest('webserver/public/build'));
+});
+
+
 gulp.task('watch', function () {
-    gulp.watch('./webserver/public/*/*', ['js']);
+    gulp.watch('./webserver/public/js/*', ['js', 'bower-js']);
 });
