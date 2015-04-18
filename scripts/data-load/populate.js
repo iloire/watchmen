f81b9200c817c9f37ac64a6ecdd9b9689468fa06
Ivan Loire <iloire@atlassian.com>
2015-04-18 19:46:20 +1000

    Fix memory leak

--- scripts/data-load/populate.js
+++ scripts/data-load/populate.js
@@ -1,69 +1,106 @@
+var async = require('async');
+
+var debug = require('debug')('data-load');
+
 var watchmen = require('../../lib/watchmen.js');
+//var redis_storage_class = require ('./../../test/lib/mock/storage_mocked');
 var redis_storage_class = require('../../lib/storage/providers/redis');
 var mocked_ping_service = require('../../test/lib/mock/request_mocked');
-var sinon = require('sinon');
-var async = require('async');
 var service_loader = require('../../lib/services');
 
-var PING_INTERVAL = 1000 * 60; // ms
-var NUMBER_PINGS_BACK = 15000;
+var DEFAULT_PING_INTERVAL = 1000 * 60 * 1; // ms
+var DEFAULT_NUMBER_DAYS_BACK = 90;
+var DEFAULT_TARGET_UPTIME = 0.99;
+var DEFAULT_TARGET_WARNING_PERCENTAGE = 0.02;
 
 var redis_storage = new redis_storage_class({port: 1216, host: '127.0.0.1', db: 1});
 var _watchmen = new watchmen(null, redis_storage);
 
 function getRandomResponse(service) {
-  var err = Math.random() > 0.98 ? 'Error connecting with server - message' : null;
+  var err = Math.random() >= DEFAULT_TARGET_UPTIME ? 'Error connecting with server - message' : null;
   var response = { statusCode: err ? 500 : 200 };
-  var latency = (service.warning_if_takes_more_than - 500) + 700 * Math.random();
+  var latency;
+  if (Math.random() <= DEFAULT_TARGET_WARNING_PERCENTAGE) { // warning
+    latency = Math.round((service.warning_if_takes_more_than) + (Math.random() * service.warning_if_takes_more_than));
+  } else{
+    latency = Math.round(service.warning_if_takes_more_than - service.warning_if_takes_more_than * Math.random());
+  }
   return { error: err, body: 'body', response: response, timeDiff: latency };
 }
 
-function getPingData(service, n) {
-  var currentTimestamp = +new Date() - PING_INTERVAL * n;
-  var data = [];
-  for (var i = 0; i < n; i++) {
-    data.push({
-      response: getRandomResponse(service),
-      timestamp: currentTimestamp + PING_INTERVAL
-    });
-    currentTimestamp += PING_INTERVAL;
-  }
-  return data;
-}
-
 function generateDataForService(service, callback) {
-  function ping(pingData, cb) {
-    sinon.useFakeTimers(pingData.timestamp);
-    service.ping_service = mocked_ping_service;
 
-    mocked_ping_service.mocked_response = pingData.response;
-    _watchmen.ping({ service: service }, function (err) {
-      cb(err);
+  var numberPingsBack = DEFAULT_NUMBER_DAYS_BACK * 1000 * 60 * 60 * 24 / DEFAULT_PING_INTERVAL;
+  debug(numberPingsBack + ' pings back calculated for ' + service.url_info);
+  var currentTimestamp = +new Date() - DEFAULT_PING_INTERVAL * numberPingsBack;
+  debug('historical data starting at ' + new Date(currentTimestamp));
+  var currentPing=0;
+
+  service.ping_service = mocked_ping_service;
+
+  for (var i = 0; i < numberPingsBack; i++) {
+    mocked_ping_service.mocked_response = getRandomResponse(service);
+
+    (function(timestamp){
+    _watchmen.ping({ timestamp: timestamp, service: service }, function (err) {
+      if (err) { return callback(err); }
+      if (currentPing % 1000 == 0) {
+        debug(new Date(timestamp), timestamp);
+        debug('processing ', currentPing);
+      }
+      currentPing++;
+      if (currentPing == numberPingsBack) {
+          callback(null);
+      }
+    });
+    })(currentTimestamp);
+
+    currentTimestamp += DEFAULT_PING_INTERVAL;
+  }
+}
+
+function run (programOptions) {
+
+  function populatedata(services, cb){
+    debug('database flushed');
+
+    async.eachSeries(services, generateDataForService, function (err, results) {
+      if (err) {
+        console.error(err);
+      }
+      redis_storage.quit(cb);
     });
   }
 
-  var pingData = getPingData(service, NUMBER_PINGS_BACK);
-  async.eachSeries(pingData, ping, function (err, results) {
+  // main
+  service_loader.load_services(function (err, services) {
     if (err) {
-      console.error(err);
+      console.error('error loading services'.red);
+      process.exit(1);
     }
-    else {
-      console.log('Data generated for service : ' + service.url_info)
+
+    if (programOptions.filter){
+      services = services.filter(function(s){
+        return s.url_info.indexOf(programOptions.filter) > -1;
+      });
     }
-    callback(err);
+
+    debug('program started');
+
+    if (!programOptions.append){
+      redis_storage.flush_database(function(){
+        populatedata(services);
+      });
+    } else{
+      populatedata(services);
+    }
   });
 }
 
-// main
-service_loader.load_services(function (err, services) {
-  if (err) {
-    console.error('error loading services'.red);
-    exit(1);
-  }
-  redis_storage.flush_database(function () {
-    async.eachSeries(services, generateDataForService, function () {
-      redis_storage.quit();
-    });
-  });
-});
+var program = require('commander');
+program
+    .option('-f, --filter [filter]', 'Filter services to add dummy data to (by url_info)')
+    .option('-a, --append', 'Do not reset database first. Be aware it may lead you to inconsistent states.')
+    .parse(process.argv);
 
+run (program);
