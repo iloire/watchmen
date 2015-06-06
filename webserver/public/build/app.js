687f41cba4d88dac556226ceda91fb9fcd64749e
Ivan Loire <iloire@atlassian.com>
2015-06-06 14:19:38 +1000

    Remember "filter to me" state (localStorage)

--- webserver/public/build/app.js
+++ webserver/public/build/app.js
@@ -72,7 +72,7 @@
 
   var SAVE_PARAMS_IN_LOCALSTORAGE = true;
 
-  angular.module('watchmenFactories').factory('ngTableUtils', function(ngTableParams) {
+  angular.module('watchmenFactories').factory('ngTableUtils', function (ngTableParams) {
 
     /**
      * Returns local stored or default parameters for ngTable.
@@ -106,12 +106,10 @@
               var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
               $defer.resolve(orderedData);
 
-              var paramsForStorage = {
-                sorting: params.sorting()
-              };
-
               if (window.localStorage) {
-                window.localStorage.setItem(key, JSON.stringify(paramsForStorage));
+                window.localStorage.setItem(key, JSON.stringify({
+                  sorting: params.sorting()
+                }));
               }
             }
           });
@@ -510,80 +508,97 @@ angular.module('watchmenControllers', []);
   var watchmenControllers = angular.module('watchmenControllers');
 
   watchmenControllers.controller('ServiceListCtrl',
-      ['$scope', '$filter', '$timeout', 'Report', 'Service', 'usSpinnerService', 'ngTableUtils',
-        function ($scope, $filter, $timeout, Report, Service, usSpinnerService, ngTableUtils) {
+      function ($scope, $filter, $timeout, Report, Service, usSpinnerService, ngTableUtils) {
 
-          var key = 'tableServicesData';
-          $scope[key] = [];
-          $scope.tableParams = ngTableUtils.createngTableParams(key, $scope, $filter);
+        var key = 'tableServicesData';
+        $scope[key] = [];
+        $scope.tableParams = ngTableUtils.createngTableParams(key, $scope, $filter);
 
-          function scheduleNextTick() {
-            $timeout.cancel(timer);
-            timer = $timeout(function () {
-              reload(scheduleNextTick, loadServicesErrHandler);
-            }, SERVICES_POLLING_INTERVAL);
-          }
+        var filterToMeCheckboxIsPresent = document.getElementById('filterRestrictedToMe');
+        if (filterToMeCheckboxIsPresent && window.localStorage) {
+          var filterToMeStoredValue = (window.localStorage.getItem('filterRestrictedToMe') === 'true');
+          $timeout(function(){
+            filterToMeCheckboxIsPresent.checked = filterToMeStoredValue;
+            $scope.filterRestrictedToMe = filterToMeStoredValue;
+          }, 0);
+        }
 
-          function loadServicesErrHandler(err) {
-            $scope.errorLoadingServices = "Error loading data from remote server";
-            console.error(err);
-            scheduleNextTick();
-          }
-
-          function reload(doneCb, errorHandler) {
-            $scope.services = Report.query(function (services) {
-              $scope[key] = services;
-              $scope.tableParams.reload();
-
-              $scope.errorLoadingServices = null; // reset error
-              transition.loaded();
-              doneCb();
-            }, errorHandler);
-          }
-
-          var transition = {
-            loading: function () {
-              usSpinnerService.spin('spinner-1');
-              $scope.loading = true;
-            },
-            loaded: function () {
-              usSpinnerService.stop('spinner-1');
-              $scope.loading = false;
+        $scope.$watch('filterRestrictedToMe',
+            function (newValue) {
+              if (window.localStorage) {
+                window.localStorage.setItem('filterRestrictedToMe', newValue);
+              }
             }
-          };
+        );
 
-          transition.loading();
+        function scheduleNextTick() {
+          $timeout.cancel(timer);
+          timer = $timeout(function () {
+            reload(scheduleNextTick, loadServicesErrHandler);
+          }, SERVICES_POLLING_INTERVAL);
+        }
 
-          $scope.serviceFilter = function (row) {
-            if ($scope.filterRestrictedToMe && !row.service.restrictedTo) {
-              return false;
-            }
-            return row.service.name.indexOf($scope.query || '') > -1;
-          };
+        function loadServicesErrHandler(err) {
+          $scope.errorLoadingServices = "Error loading data from remote server";
+          console.error(err);
+          scheduleNextTick();
+        }
 
-          $scope.delete = function (id) {
-            if (confirm('Are you sure you want to delete this service and all its data?')) {
-              Service.delete({id: id}, function () {
-                reload(function () {
-                }, function () {
-                  $scope.errorLoadingServices = "Error loading data from remote server";
-                });
+        function reload(doneCb, errorHandler) {
+          $scope.services = Report.query(function (services) {
+            $scope[key] = services;
+            $scope.tableParams.reload();
+
+            $scope.errorLoadingServices = null; // reset error
+            transition.loaded();
+            doneCb();
+          }, errorHandler);
+        }
+
+        var transition = {
+          loading: function () {
+            usSpinnerService.spin('spinner-1');
+            $scope.loading = true;
+          },
+          loaded: function () {
+            usSpinnerService.stop('spinner-1');
+            $scope.loading = false;
+          }
+        };
+
+        transition.loading();
+
+        $scope.serviceFilter = function (row) {
+          if ($scope.filterRestrictedToMe && !row.service.restrictedTo) {
+            return false;
+          }
+          return row.service.name.indexOf($scope.query || '') > -1;
+        };
+
+        $scope.delete = function (id) {
+          if (confirm('Are you sure you want to delete this service and all its data?')) {
+            Service.delete({id: id}, function () {
+              reload(function () {
+              }, function () {
+                $scope.errorLoadingServices = "Error loading data from remote server";
               });
-            }
-          };
+            });
+          }
+        };
 
-          $scope.reset = function (id) {
-            if (confirm('Are you sure you want to reset this service\'s data?')) {
-              Service.reset({id: id}, function () {
-                reload(function () {
-                }, function () {
-                  $scope.errorLoadingServices = "Error loading data from remote server";
-                });
+        $scope.reset = function (id) {
+          if (confirm('Are you sure you want to reset this service\'s data?')) {
+            Service.reset({id: id}, function () {
+              reload(function () {
+              }, function () {
+                $scope.errorLoadingServices = "Error loading data from remote server";
               });
-            }
-          };
+            });
+          }
+        };
 
-          reload(scheduleNextTick, loadServicesErrHandler);
+        reload(scheduleNextTick, loadServicesErrHandler);
 
-        }]);
+      });
+
 })();
