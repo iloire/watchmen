645c4e5dc989b4c25fa826559e67740f78e0f7c3
Ivan Loire <iloire@atlassian.com>
2015-06-13 19:14:03 +1000

    Resize charts on window resize

--- webserver/public/js/controllers/service-details.js
+++ webserver/public/js/controllers/service-details.js
@@ -12,6 +12,10 @@
   watchmenControllers.controller('ServiceDetailCtrl', ['$scope', '$filter', '$stateParams', 'Report', 'ngTableUtils', 'usSpinnerService', '$timeout',
     function ($scope, $filter, $stateParams, Report, ngTableUtils, usSpinnerService, $timeout) {
 
+      function getChartSize() {
+        return {height: 150, width: $('.view-frame').width()};
+      }
+
       function loading(){
         usSpinnerService.spin('spinner-1');
         $scope.loading = true;
@@ -57,38 +61,37 @@
         });
 
         var max = _.max([maxLastHour.l, maxLast24Hours.l, maxLastWeek.l]);
-        var defaultChartWidth = $('.view-frame').width();
-        var chartSize = {height: 150, width: defaultChartWidth};
+        var chartSize = getChartSize();
 
+        var charts = [];
         $timeout(function () {
-
           if (latencyLastHour.list.length > 0) { // at least one successful ping
             $scope.showLastHourChart = true;
-            Charting.render({
+            charts.push(Charting.render({
               threshold: data.service.warningThreshold,
               latency: latencyLastHour.list,
               outages: data.status.lastHour.outages,
               id: '#chart-last-hour',
               size: chartSize,
               max: max
-            });
+            }));
           }
 
           if (latencyLast24Hours.list.length > 8) {
             $scope.showLast24Chart = true;
-            Charting.render({
+            charts.push(Charting.render({
               threshold: data.service.warningThreshold,
               latency: latencyLast24Hours.list,
               outages: data.status.last24Hours.outages,
               id: '#chart-last-24-hours',
               size: chartSize,
               max: max
-            });
+            }));
           }
 
           if (latencyLastWeek.list.length > 1) {
             $scope.showLastWeekChart = true;
-            Charting.render({
+            charts.push(Charting.render({
               threshold: data.service.warningThreshold,
               latency: latencyLastWeek.list,
               outages: data.status.lastWeek.outages,
@@ -96,8 +99,15 @@
               size: chartSize,
               x_format: '%d/%m',
               max: max
-            });
+            }));
           }
+
+          $(window).resize(function(){
+            for (var i = 0; i < charts.length; i++) {
+              charts[i].resize(getChartSize());
+            }
+          });
+
         }, 0);
       }, errHandler);
 
