82888ccf6008422b98d5765ce2db34db928281e9
Ivan Loire <iloire@atlassian.com>
2015-06-13 15:01:52 +1000

    Sentinel restarts modified services

--- test/test-sentinel.js
+++ test/test-sentinel.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 
 describe('sentinel', function () {
 
-  it('it should find new Services', function () {
+  it('should find new Services', function () {
     var dbServices = [{id: 'Eg34'}, {id: 2}, {id: '33'}];
     var runningServices = [{id: 'Eg34'}, {id: 2}];
     var sentinel = new sentinelFactory([], null);
@@ -11,12 +11,27 @@ describe('sentinel', function () {
     assert.equal(addedServices.length, 1);
   });
 
-  it('it should find removed Services', function () {
+  it('should find removed Services', function () {
     var dbServices = [{id: 'Eg34'}, {id: 2}];
     var runningServices = [{id: 'Eg34'}, {id: 2}, {id: '98989'}, {id: '33'}];
     var sentinel = new sentinelFactory([], null);
-    var addedServices = sentinel._findRemoved(dbServices, runningServices);
-    assert.equal(addedServices.length, 2);
+    var removedServices = sentinel._findRemoved(dbServices, runningServices);
+    assert.equal(removedServices.length, 2);
   });
 
+  it('should find modified Services', function () {
+    var dbServices = [{id: '1', interval: 2000}, {id: '2', interval: 3000}, {id: '3', interval: 4000}];
+    var runningServices = [{id: '1', interval: 2300}, {id: '2', interval: 3000}, {id: '3', interval: 4000}];
+    var sentinel = new sentinelFactory([], null);
+    var modifiedServices = sentinel._findModified(dbServices, runningServices);
+    assert.equal(modifiedServices.length, 1);
+  });
+
+  it('should filter ignored fields', function () {
+    var dbServices = [{id: '1', pingService: 'val1'}, {id: '2', pingService: 'val1'}];
+    var runningServices =  [{id: '1', pingService: 'val1'}, {id: '2', pingService: 'val2'}];
+    var sentinel = new sentinelFactory([], null);
+    var modifiedServices = sentinel._findModified(dbServices, runningServices);
+    assert.equal(modifiedServices.length, 0);
+  });
 });
