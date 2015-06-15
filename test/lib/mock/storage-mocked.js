0c2518ef384989214d6c6ffcce947f8b07bd4cb4
Ivan Loire <iloire@atlassian.com>
2015-06-15 22:10:13 +1000

    Use setImmediate instead of process.nextTick (since 0.10)

--- test/lib/mock/storage-mocked.js
+++ test/lib/mock/storage-mocked.js
@@ -13,33 +13,33 @@ exports = module.exports = StorageMocked;
 
 StorageMocked.prototype.startOutage = function (service, outageData, callback) {
   this.currentOutage = outageData;
-  process.nextTick(function(){
+  setImmediate(function(){
     callback(null);
   });
 };
 
 StorageMocked.prototype.getCurrentOutage = function (service, callback) {
   var self = this;
-  process.nextTick(function(){
+  setImmediate(function(){
     callback(null, self.currentOutage);
   });
 };
 
 StorageMocked.prototype.saveLatency = function (service, timestamp, latency, callback) {
-  process.nextTick(function(){
+  setImmediate(function(){
     callback(null);
   });
 };
 
 StorageMocked.prototype.archiveCurrentOutageIfExists = function (service, callback) {
   var self = this;
-  process.nextTick(function(){
+  setImmediate(function(){
     callback(null, self.currentOutage);
   });
 };
 
 StorageMocked.prototype.flush_database = function (callback){
-  process.nextTick(function(){
+  setImmediate(function(){
     callback(null);
   });
 };
