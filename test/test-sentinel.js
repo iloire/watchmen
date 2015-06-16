7dcf825a1d67056f045efe1069230d49d5d9f220
Ivan Loire <iloire@atlassian.com>
2015-06-16 21:49:38 +1000

    White list fields that trigger service changes

--- test/test-sentinel.js
+++ test/test-sentinel.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 
 describe('sentinel', function () {
 
-  it('should find new Services', function () {
+  it('should find new services', function () {
     var dbServices = [{id: 'Eg34'}, {id: 2}, {id: '33'}];
     var runningServices = [{id: 'Eg34'}, {id: 2}];
     var sentinel = new sentinelFactory([], null);
@@ -11,7 +11,7 @@ describe('sentinel', function () {
     assert.equal(addedServices.length, 1);
   });
 
-  it('should find removed Services', function () {
+  it('should find removed services', function () {
     var dbServices = [{id: 'Eg34'}, {id: 2}];
     var runningServices = [{id: 'Eg34'}, {id: 2}, {id: '98989'}, {id: '33'}];
     var sentinel = new sentinelFactory([], null);
@@ -19,12 +19,20 @@ describe('sentinel', function () {
     assert.equal(removedServices.length, 2);
   });
 
-  it('should find modified Services', function () {
-    var dbServices = [{id: '1', interval: 2000}, {id: '2', interval: 3000}, {id: '3', interval: 4000}];
-    var runningServices = [{id: '1', interval: 2300}, {id: '2', interval: 3000}, {id: '3', interval: 4000}];
+  it('should find modified services when whitelisted fields change', function () {
+    var dbServices = [{id: '1', interval: 2000}, {id: '2', interval: 3000}];
+    var runningServices = [{id: '1', interval: 2300}, {id: '2', interval: 3000}];
     var sentinel = new sentinelFactory([], null);
     var modifiedServices = sentinel._findModified(dbServices, runningServices);
     assert.equal(modifiedServices.length, 1);
+  });
+
+  it('should ignore certain fields', function () {
+    var dbServices = [{id: '1', pingServiceOptions: 'options'}, {id: '2', pingServiceOptions: 'options'}];
+    var runningServices = [{id: '1', pingServiceOptions: 'change in options'}, {id: '2', pingServiceOptions: 'options'}];
+    var sentinel = new sentinelFactory([], null);
+    var modifiedServices = sentinel._findModified(dbServices, runningServices);
+    assert.equal(modifiedServices.length, 0);
   });
 
 });
