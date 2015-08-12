8d0892ccc67e5c7ec7ac94c3c51f5872ad00edd4
Luis Bianchin <labianchin@users.noreply.github.com>
2015-08-12 11:22:50 -0300

    Adds env var for redis host in storage.js

--- config/storage.js
+++ config/storage.js
@@ -5,7 +5,7 @@ module.exports = {
     options : {
       'redis' : {
         port: process.env.WATCHMEN_REDIS_PORT_PRODUCTION || 1216,
-        host: '127.0.0.1',
+        host: process.env.WATCHMEN_REDIS_ADDR_PRODUCTION || '127.0.0.1',
         db: process.env.WATCHMEN_REDIS_DB_PRODUCTION || 1
       }
     }
@@ -16,7 +16,7 @@ module.exports = {
     options : {
       'redis' : {
         port: process.env.WATCHMEN_REDIS_PORT_DEVELOPMENT || 1216,
-        host: '127.0.0.1',
+        host: process.env.WATCHMEN_REDIS_ADDR_DEVELOPMENT || '127.0.0.1',
         db: process.env.WATCHMEN_REDIS_DB_DEVELOPMENT || 2
       }
     }
@@ -27,7 +27,7 @@ module.exports = {
     options : {
       'redis' : {
         port: process.env.WATCHMEN_REDIS_PORT_TEST || 6666,
-        host: '127.0.0.1',
+        host: process.env.WATCHMEN_REDIS_ADDR_TEST || '127.0.0.1',
         db: process.env.WATCHMEN_REDIS_DB_TEST || 1
       }
     }
