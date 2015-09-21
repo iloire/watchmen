db38d4a69c8a15d25b32273b5e46c29d6bfd08b2
Ivan Loire <iloire@atlassian.com>
2015-09-21 18:36:15 +1000

    Fix indentation

--- webserver/routes/api-service-route.js
+++ webserver/routes/api-service-route.js
@@ -4,168 +4,168 @@ var debug = require('debug')('service-route');
 var serviceValidator = require('./../../lib/service-validator');
 var accessFilter = require('./../../lib/service-access-filter');
 
-module.exports.getRoutes = function (storage){
+module.exports.getRoutes = function (storage) {
 
-    if (!storage){
-        throw 'storage needed';
+  if (!storage) {
+    throw 'storage needed';
+  }
+
+  var router = express.Router();
+
+  var requireAdmin = function (req, res, next) {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(401).json({error: 'auth required'});
+    }
+  };
+
+  /**
+   * Add service
+   */
+
+  router.post('/services', requireAdmin, function (req, res) {
+    var service = req.body;
+
+    var errors = serviceValidator.validate(service);
+    if (errors.length) {
+      return res.status(400).json({errors: errors});
+    }
+    storage.addService(service, function (err, id) {
+      if (err) {
+        return res.status(500).json({error: err});
+      }
+      return res.status(200).json({id: id});
+    });
+  });
+
+  /**
+   * Delete service
+   */
+
+  router.delete('/services/:id', requireAdmin, function (req, res) {
+    var id = req.params.id;
+    if (!id) {
+      return res.status(404).json({error: 'ID parameter not found'});
+    }
+    storage.getService(id, function (err, service) {
+      if (err) {
+        return res.status(500).json({error: err});
+      }
+
+      if (!service) {
+        return res.status(404).json({error: 'service not found'});
+      }
+
+      storage.deleteService(id, function (err) {
+        if (err) {
+          return res.status(500).json({error: err});
+        }
+        return res.json({id: id});
+      });
+    });
+  });
+
+  /**
+   * Update service
+   */
+
+  router.post('/services/:id', requireAdmin, function (req, res) {
+
+    var id = req.params.id;
+    if (!id) {
+      return res.status(404).json({error: 'ID parameter not found'});
     }
 
-    var router = express.Router();
+    var errors = serviceValidator.validate(req.body);
+    if (errors.length) {
+      return res.status(400).json({errors: errors});
+    }
 
-    var requireAdmin = function(req, res, next){
-        if (req.user && req.user.isAdmin){
-            next();
-        } else {
-            return res.status(401).json({ error: 'auth required' });
+    storage.getService(id, function (err, existingService) {
+      if (err) {
+        return res.status(500).json({error: err});
+      }
+
+      if (!existingService) {
+        return res.status(404).json({error: 'service not found'});
+      }
+
+      storage.updateService(req.body, function (err, service) {
+        if (err) {
+          return res.status(500).json({error: err});
         }
-    };
-
-    /**
-     * Add service
-     */
-
-    router.post('/services', requireAdmin, function(req, res){
-        var service = req.body;
-
-        var errors = serviceValidator.validate(service);
-        if (errors.length) {
-            return res.status(400).json({ errors: errors });
-        }
-        storage.addService(service, function(err, id){
-            if (err) {
-                return res.status(500).json({ error: err });
-            }
-            return res.status(200).json({ id: id });
-        });
+        return res.json(service);
+      });
     });
+  });
 
-    /**
-     * Delete service
-     */
 
-    router.delete('/services/:id', requireAdmin, function(req, res){
-        var id = req.params.id;
-        if (!id) {
-            return res.status(404).json({ error: 'ID parameter not found' });
+  /**
+   * Reset service data
+   */
+
+  router.post('/services/:id/reset', requireAdmin, function (req, res) {
+    var id = req.params.id;
+    if (!id) {
+      return res.status(404).json({error: 'ID parameter not found'});
+    }
+    storage.getService(id, function (err, service) {
+      if (err) {
+        return res.status(500).json({error: err});
+      }
+
+      if (!service) {
+        return res.status(404).json({error: 'service not found'});
+      }
+
+      storage.resetService(id, function (err) {
+        if (err) {
+          return res.status(500).json({error: err});
         }
-        storage.getService(id, function(err, service){
-            if (err) {
-                return res.status(500).json({ error: err });
-            }
-
-            if (!service) {
-                return res.status(404).json({ error: 'service not found' });
-            }
-
-            storage.deleteService(id, function(err){
-                if (err) {
-                    return res.status(500).json({ error: err });
-                }
-                return res.json({ id: id });
-            });
-        });
+        return res.json({id: id});
+      });
     });
+  });
 
-    /**
-     * Update service
-     */
+  /**
+   * Load service
+   */
 
-    router.post('/services/:id', requireAdmin, function(req, res){
-        
-        var id = req.params.id;
-        if (!id) {
-            return res.status(404).json({ error: 'ID parameter not found' });
-        }
+  router.get('/services/:id', requireAdmin, function (req, res) {
+    if (!req.params.id) {
+      return res.status(404).json({error: 'ID parameter not found'});
+    }
+    storage.getService(req.params.id, function (err, service) {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({error: err});
+      }
 
-        var errors = serviceValidator.validate(req.body);
-        if (errors.length) {
-            return res.status(400).json({ errors: errors });
-        }
+      service = accessFilter.filterServices(service, req.user);
 
-        storage.getService(id, function(err, existingService){
-            if (err) {
-                return res.status(500).json({ error: err });
-            }
-
-            if (!existingService) {
-                return res.status(404).json({ error: 'service not found' });
-            }
-
-            storage.updateService(req.body, function(err, service){
-                if (err) {
-                    return res.status(500).json({ error: err });
-                }
-                return res.json(service);
-            });
-        });
+      if (!service) {
+        return res.status(404).json({error: 'Service not found'});
+      }
+      res.json(service);
     });
+  });
 
+  /**
+   * Load services
+   */
 
-    /**
-     * Reset service data
-     */
-
-    router.post('/services/:id/reset', requireAdmin, function(req, res){
-        var id = req.params.id;
-        if (!id) {
-            return res.status(404).json({ error: 'ID parameter not found' });
-        }
-        storage.getService(id, function(err, service){
-            if (err) {
-                return res.status(500).json({ error: err });
-            }
-
-            if (!service) {
-                return res.status(404).json({ error: 'service not found' });
-            }
-
-            storage.resetService(id, function(err){
-                if (err) {
-                    return res.status(500).json({ error: err });
-                }
-                return res.json({ id: id });
-            });
-        });
+  router.get('/services', requireAdmin, function (req, res) {
+    storage.getServices({}, function (err, services) {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({error: err});
+      }
+      // for small number of services (hundreds) we can go away with post database query filtering.
+      // if you are managing thousands of services you may to index users/services for better performance
+      res.json(accessFilter.filterServices(services, req.user));
     });
+  });
 
-    /**
-     * Load service
-     */
-
-    router.get('/services/:id', requireAdmin, function(req, res){
-        if (!req.params.id) {
-            return res.status(404).json({ error: 'ID parameter not found' });
-        }
-        storage.getService(req.params.id, function (err, service){
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ error: err });
-            }
-
-            service = accessFilter.filterServices(service, req.user);
-
-            if (!service) {
-                return res.status(404).json({ error: 'Service not found' });
-            }
-            res.json(service);
-        });
-    });
-
-    /**
-     * Load services
-     */
-
-    router.get('/services', requireAdmin, function(req, res){
-        storage.getServices({}, function (err, services){
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ error: err });
-            }
-            // for small number of services (hundreds) we can go away with post database query filtering.
-            // if you are managing thousands of services you may to index users/services for better performance
-            res.json(accessFilter.filterServices(services, req.user));
-        });
-    });
-
-    return router;
+  return router;
 };
